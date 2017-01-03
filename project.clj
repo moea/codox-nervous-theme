4082beac26ecfa7148f60f12e360b7e7c0242dd5
Moe Aboulkheir <moe@nervous.io>
2017-01-03 14:03:46 +0000

    Initial commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject io.nervous/codox-nervous-theme "0.1.0"
+  :description "Nervous Systems Codox theme"
+  :url         "https://github.com/nervous-systems/codox-nervous-theme"
+  :license     {:name "Unlicense" :url "http://unlicense.org/UNLICENSE"}
+  :scm         {:name "git" :url "https://github.com/nervous-systems/codox-nervous-theme"})
